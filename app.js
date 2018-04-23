efa4ea30a0d29291e0d84e77dc20417cd151bc45
Realized-Technologies <Rcolon03@gmail.com>
2018-04-23 18:23:14 +0000

    Add mongolab and update stylesheet

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_version18");
+// mongoose.connect("mongodb://localhost/yelp_camp_version18");
+mongoose.connect("mongodb://Reynolds:Precioso@ds149724.mlab.com:49724/proj_yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
